892ad8c9f69511400b409e6ec423f9824528ffd4
roryg11 <Rory.C.Grant@gmail.com>
2015-02-26 15:03:26 -0800

    working loading of teams and players

--- app/controllers/teams.js
+++ app/controllers/teams.js
@@ -2,6 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   newTeamOnePlayers: "",
+  newTeamTwoPlayers: "",
+  teamOneShow: [],
+  teamTwoShow: [],
+  teamOneArray: [],
+  teamTwoArray: [],
+  teamTwoPlayerStats: [],
+  teamOnePlayerStats: [],
   foundTeamsOne: function() {
     var teams = this.get('model');
     var filterString = this.get('newTeamOnePlayers').toLowerCase();
@@ -9,12 +16,47 @@ export default Ember.Controller.extend({
       return team.get('name').toLowerCase().indexOf(filterString) >= 0;
     });
   }.property('newTeamOnePlayers', 'model.@each.name'),
-  newTeamTwoPlayers: "",
   foundTeamsTwo: function() {
     var teams = this.get('model');
     var filterString = this.get('newTeamTwoPlayers').toLowerCase();
     return teams.filter(function(team){
       return team.get('name').toLowerCase().indexOf(filterString) >= 0;
     });
-  }.property('newTeamTwoPlayers', 'model.@each.name')
+  }.property('newTeamTwoPlayers', 'model.@each.name'),
+  actions: {
+    showTeamOne: function(team){
+      var _this = this;
+      var team= team._data
+      this.set('teamOneShow', team);
+      this.set('newTeamOnePlayers', '');
+    },
+    showTeamTwo: function(team){
+      var _this = this;
+      var team= team._data
+      this.set('teamTwoShow', team);
+      this.set('newTeamTwoPlayers', '');
+    },
+    findTeamTwoPlayers: function(team){
+      var _this=this;
+      Ember.$.getJSON('https://safe-gorge-4257.herokuapp.com/teams/' + team).then(function(response){
+        var team = response.teams;
+        _this.set('teamTwoArray', team);
+      })
+    },
+    findTeamOnePlayers: function(team){
+      var _this=this;
+      Ember.$.getJSON('https://safe-gorge-4257.herokuapp.com/teams/' + team).then(function(response){
+        var team = response.teams;
+        _this.set('teamOneArray', team);
+      })
+    },
+    showTeamTwoPlayersStats: function(player){
+      this.set('teamTwoPlayerStats', player);
+      this.set('teamTwoArray', '');
+    },
+    showTeamOnePlayersStats: function(player){
+      this.set('teamOnePlayerStats', player);
+      this.set('teamOneArray', '');
+    }
+  }
 });
